da9de6524df1d64111e3c8eeea68c8310567c94d
Phil Liao <phil@0x.org>
2022-03-23 18:22:47 -0700

    add styles, hardcode in borrow page

--- website/pages/borrow.tsx
+++ website/pages/borrow.tsx
@@ -3,12 +3,17 @@ import { NextPage } from 'next';
 
 import { H1 } from '../components/Typography';
 import { PageContainer } from '../components/Layout';
+import { NFTView } from '../components/NFTView';
 
 const HomePage: NextPage = () => {
   return (
     <>
       <PageContainer style={{ position: 'relative' }} id="top">
         <H1> Hello Borrower</H1>
+        <NFTView
+          contract="0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
+          tokenId={100}
+        ></NFTView>
       </PageContainer>
     </>
   );
